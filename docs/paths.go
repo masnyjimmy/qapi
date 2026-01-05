a958ed46823b2229632f1f329bf35f8b8e3e559f
masnyjimmy <kspark470@gmail.com>
2026-01-05 12:29:27 +0100

    Fixed Paths unmarshalling

--- docs/paths.go
+++ docs/paths.go
@@ -1,5 +1,7 @@
 package docs
 
+import "github.com/goccy/go-yaml"
+
 type Path struct {
 	Tags   []string        `yaml:"tags,omitempty"`
 	Get    *Method         `yaml:"get,omitempty"`
@@ -10,3 +12,65 @@ type Path struct {
 }
 
 type Paths = map[string]Path
+
+func (p *Path) UnmarshalYAML(bytes []byte) error {
+
+	var raw map[string]yaml.RawMessage
+
+	if err := yaml.Unmarshal(bytes, &raw); err != nil {
+		return err
+	}
+
+	if tags, has := raw["tags"]; has {
+		if err := yaml.Unmarshal(tags, &p.Tags); err != nil {
+			return err
+		}
+		delete(raw, "tags")
+	}
+
+	if get, has := raw["get"]; has {
+		if err := yaml.Unmarshal(get, &p.Get); err != nil {
+			return err
+		}
+		delete(raw, "get")
+	}
+
+	if put, has := raw["put"]; has {
+		if err := yaml.Unmarshal(put, &p.Put); err != nil {
+			return err
+		}
+		delete(raw, "put")
+	}
+
+	if post, has := raw["post"]; has {
+		if err := yaml.Unmarshal(post, &p.Post); err != nil {
+			return err
+		}
+		delete(raw, "post")
+	}
+
+	if del, has := raw["delete"]; has {
+		if err := yaml.Unmarshal(del, &p.Delete); err != nil {
+			return err
+		}
+		delete(raw, "delete")
+	}
+
+	if len(raw) == 0 {
+		return nil
+	} else {
+		p.Nested = make(map[string]Path)
+	}
+
+	var outPath Path
+
+	for k, v := range raw {
+		if err := yaml.Unmarshal(v, &outPath); err != nil {
+			return err
+		}
+		p.Nested[k] = outPath
+	}
+
+	return nil
+
+}
