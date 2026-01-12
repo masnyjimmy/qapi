66202cf537b9f6bb9dee6e76d5735be5add91984
masnyjimmy <kspark470@gmail.com>
2026-01-12 14:30:28 +0100

    .

--- compilation/compiler.go
+++ compilation/compiler.go
@@ -378,9 +378,11 @@ func (c *CompileContext) parseMethod(method *docs.Method, tags []string, path st
 
 			for mediaType, schema := range response.TypedSchema {
 				outSchema, err := c.ParseSchema(schema)
+
 				if err != nil {
 					return nil, err
 				}
+
 				outResponse.Content[mediaType] = TypedSchema{
 					Schema: outSchema,
 				}
